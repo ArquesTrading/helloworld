153e85a69009b70ea5b013f79f2c632704108e45
arques-insukim <insukim@arques.com>
2020-11-26 14:32:21 +0900

    멀티 서버 배포 테스트

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
     except:
         pass
 
-    data = {"message": "Hello Arqeus By Cloud Build And Github Actions", "time": time}
+    data = {"message": "Hello Arqeus Multi-Servers By Cloud Build And Github Actions", "time": time}
     return jsonify(data)
 
 
